b3a7ce1c217664ad407c91513b1d531b53c946f5
vx-nu <vishnuus934@gmail.com>
2025-02-23 10:21:36 +0530

    Delete src directory

--- src/main/java/com/vishnuus/task_api/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vishnuus.task_api.repository; 
-
-import com.vishnuus.task_api.model.Task;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface TaskRepository extends MongoRepository<Task, String> {
-    List<Task> findByNameContaining(String name);
-}
